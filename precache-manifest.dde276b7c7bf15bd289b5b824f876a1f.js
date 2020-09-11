eea4fc13b3d67e50d03a47242711dea3520a9add
bamboleylo <tsybulko.artem@gmail.com>
2020-09-11 09:46:38 +0300

    Updates

---
+++ precache-manifest.dde276b7c7bf15bd289b5b824f876a1f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93d5e7e42e6699fbce10363d2e2960b0",
+    "url": "/my-image-wall/index.html"
+  },
+  {
+    "revision": "098fc2ce593d2f9d0e98",
+    "url": "/my-image-wall/static/css/main.aafd8057.chunk.css"
+  },
+  {
+    "revision": "ef2f25277d4fe273b7fc",
+    "url": "/my-image-wall/static/js/2.0427e9d7.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/my-image-wall/static/js/2.0427e9d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "098fc2ce593d2f9d0e98",
+    "url": "/my-image-wall/static/js/main.9cc2b783.chunk.js"
+  },
+  {
+    "revision": "57fb8b065994c49c60da",
+    "url": "/my-image-wall/static/js/runtime-main.5b51e2b0.js"
+  },
+  {
+    "revision": "f1e994695c467c93c2efacec6de66d26",
+    "url": "/my-image-wall/static/media/loader.f1e99469.svg"
+  }
+]);
